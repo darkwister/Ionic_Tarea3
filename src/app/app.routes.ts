f1c4a6d3c5a41c926a8a5dc3a38761ebba8b46a8
DarkWister <darkwistercrak17@gmail.com>
2025-02-04 16:58:16 -0400

    added main routes of the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: 'folder/my_info',
     pathMatch: 'full',
   },
   {
     path: 'folder/:id',
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'my-info',
+    loadComponent: () => import('./pages/my-info/my-info.page').then( m => m.MyInfoPage)
   },
 ];
