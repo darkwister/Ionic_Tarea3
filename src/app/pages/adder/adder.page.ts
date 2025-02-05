2917bc7ffaf713f336afe4d880e380173a480381
DarkWister <darkwistercrak17@gmail.com>
2025-02-04 21:19:07 -0400

    Suma funcional

--- src/app/pages/adder/adder.page.ts
+++ src/app/pages/adder/adder.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonImg } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonImg, IonInput, IonButton } from '@ionic/angular/standalone';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -9,13 +9,20 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './adder.page.html',
   styleUrls: ['./adder.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonicModule, IonLabel, IonImg]
 })
 export class AdderPage implements OnInit {
+  num1: number = 0;  
+  num2: number = 0;
+  result?: number;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  sumar(num1: any, num2: any): number {
+    this.result = Number(num1) + Number(num2); 
+    return this.result;
+  }
 }
