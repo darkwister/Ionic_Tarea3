1ce6afa246ea66fc3f9275deb70055cb86a4c5f3
DarkWister <darkwistercrak17@gmail.com>
2025-02-05 01:13:21 -0400

    added joke and placeholder video

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,13 @@ const config: CapacitorConfig = {
   appId: 'io.darkwister.Test1',
   appName: 'Ionic Wister',
   webDir: 'www'
-};
+,
+    android: {
+       buildOptions: {
+          keystorePath: 'd:\Files\ApkKey.jks',
+          keystoreAlias: 'DarkWister',
+       }
+    }
+  };
 
 export default config;
