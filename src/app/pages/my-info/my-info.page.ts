bbb48ba9abc62d7d439d93b5e433f78d46355c3f
DarkWister <darkwistercrak17@gmail.com>
2025-02-04 20:13:06 -0400

    My info is done

--- src/app/pages/my-info/my-info.page.ts
+++ src/app/pages/my-info/my-info.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonImg } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-my-info',
   templateUrl: './my-info.page.html',
   styleUrls: ['./my-info.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel, IonImg, IonicModule]
 })
 export class MyInfoPage implements OnInit {
 
