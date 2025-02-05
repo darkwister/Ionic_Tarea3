f53ea6943834b7dde21f2a5c4c83ef77dd8807a3
DarkWister <darkwistercrak17@gmail.com>
2025-02-04 22:22:36 -0400

    optimized imports

--- src/app/pages/adder/adder.page.ts
+++ src/app/pages/adder/adder.page.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonImg, IonInput, IonButton, IonList } from '@ionic/angular/standalone';
-import { IonicModule } from '@ionic/angular';
-import { empty } from 'rxjs';
+import { IonTextarea, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonInput, IonButton, IonList } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-adder',
   templateUrl: './adder.page.html',
   styleUrls: ['./adder.page.scss'],
   standalone: true,
-  imports: [IonList, IonButton, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonicModule, IonLabel, IonImg]
+  imports: [IonTextarea, IonItem, IonList, IonButton, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel ]
 })
 export class AdderPage{
   num1?: number;  
--- src/app/pages/my-info/my-info.page.ts
+++ src/app/pages/my-info/my-info.page.ts
@@ -1,20 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonImg, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
-import { IonicModule } from '@ionic/angular';
+import { IonList, IonItem ,IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonImg, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-my-info',
   templateUrl: './my-info.page.html',
   styleUrls: ['./my-info.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel, IonImg, IonicModule,IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle]
+  imports: [IonList, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel, IonImg,IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle]
 })
-export class MyInfoPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-}
+export class MyInfoPage {}
