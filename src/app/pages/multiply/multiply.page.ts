e202d0851511d0737adcecfa430c3de65700c391
DarkWister <darkwistercrak17@gmail.com>
2025-02-04 23:55:08 -0400

    done, video missing

--- src/app/pages/multiply/multiply.page.ts
+++ src/app/pages/multiply/multiply.page.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonImg } from '@ionic/angular/standalone';
-import { IonicModule } from '@ionic/angular';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonItem, IonList, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonInput} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-multiply',
   templateUrl: './multiply.page.html',
   styleUrls: ['./multiply.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent, IonHeader, IonTitle, IonToolbar, 
+    CommonModule, FormsModule, IonLabel, IonItem, 
+    IonList, IonButton, IonCard, IonCardContent, 
+    IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput
+  ]
 })
-export class MultiplyPage implements OnInit {
+export class MultiplyPage {
+  inputNumber: number = 0;
+  lastNumber: number = 0;
+  resultados: { num: number, resultado: number }[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  multiplicar() {
+    this.resultados = [];
+    if (this.inputNumber) {
+      this.lastNumber = this.inputNumber;
+      for (let i = 1; i <= 13; i++) {
+        this.resultados.push({ num: i, resultado: this.lastNumber * i });
+      }
+    }
   }
-
 }
