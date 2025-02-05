de73c9af439dc0d97836259dc5288a5e97fbdec2
DarkWister <darkwistercrak17@gmail.com>
2025-02-04 22:12:47 -0400

    CSS and Pullished method

--- src/app/pages/adder/adder.page.ts
+++ src/app/pages/adder/adder.page.ts
@@ -1,25 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonImg, IonInput, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonImg, IonInput, IonButton, IonList } from '@ionic/angular/standalone';
 import { IonicModule } from '@ionic/angular';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-adder',
   templateUrl: './adder.page.html',
   styleUrls: ['./adder.page.scss'],
   standalone: true,
-  imports: [IonButton, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonicModule, IonLabel, IonImg]
+  imports: [IonList, IonButton, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonicModule, IonLabel, IonImg]
 })
-export class AdderPage implements OnInit {
+export class AdderPage{
   num1?: number;  
   num2?: number;
   result?: number;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   sumar(num1: any, num2: any): number {
     this.result = Number(num1) + Number(num2); 
