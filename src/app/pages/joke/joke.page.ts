e598c01aa7069aa333cfb34bd9d97f61b8645fd1
DarkWister <darkwistercrak17@gmail.com>
2025-02-05 01:41:36 -0400

    changed imports and added list to joke

--- src/app/pages/joke/joke.page.ts
+++ src/app/pages/joke/joke.page.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonImg } from '@ionic/angular/standalone';
-import { IonicModule } from '@ionic/angular';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonList, IonItem} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-joke',
   templateUrl: './joke.page.html',
   styleUrls: ['./joke.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel, IonList, IonItem]
 })
 export class JokePage {}
--- src/app/pages/video/video.page.ts
+++ src/app/pages/video/video.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.page.html',
   styleUrls: ['./video.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel]
 })
 export class VideoPage {}
