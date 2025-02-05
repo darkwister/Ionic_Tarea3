9a3903673a115ebafac66ad5e0f20fd768f0b403
DarkWister <darkwistercrak17@gmail.com>
2025-02-05 00:38:03 -0400

    configs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Test1',
+  appId: 'io.darkwister.Test1',
+  appName: 'Ionic Wister',
   webDir: 'www'
 };
 
